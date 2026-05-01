5e3070870340b317401e424fed346cc8cae40478
perascro <perascro@proton.me>
2026-05-02 02:03:50 +0800

    更新 n no limit）.c

--- dream1-1-4（sum of 1/n no limit）.c
+++ dream1-1-4（sum of 1/n no limit）.c
@@ -2,10 +2,12 @@
 //this code is to prove the sum of series 1/n do not have limit 
 #include<stdio.h>
 #include<math.h>
+#include<time.h>
 double plus(double n);
 int main()
 {
-double n,s;
+double n,s,start,end;
+n=0;
 for(int t=1;t>0;t++)
 {
 printf("enter n.\n");
@@ -15,8 +17,12 @@ t=-1;
 }
 else
 {
+
 scanf("%lf",&n);
+start=clock();
 plus(n);
+end=clock();
+printf("run time [%lf] seconds\n",(end-start)/CLOCKS_PER_SEC);
 }
 }
 return 0;
